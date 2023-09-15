79c95c7714560a145c8bc0e7f1415e1e84f5239b
unknown <ictnenad@gmail.com>
2023-09-15 13:29:20 +0200

    commit

---
+++ iam_scanner.py
@@ -0,0 +1,8 @@
+import os
+import boto3
+from tabulate import tabulate
+
+aws_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
+aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
+aws_region = os.environ["AWS_REGION"]
+# ... rest of your script remains the same ...
